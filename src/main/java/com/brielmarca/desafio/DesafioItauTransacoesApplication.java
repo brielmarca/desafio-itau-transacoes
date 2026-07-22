3b5100fe1fc1b2c671758874ad962a4015e12de3
brielmarca <brielmarca@gmail.com>
2026-07-22 17:16:22 -0300

    chore: inicializa projeto Spring Boot

---
+++ src/main/java/com/brielmarca/desafio/DesafioItauTransacoesApplication.java
@@ -0,0 +1,13 @@
+package com.brielmarca.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioItauTransacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioItauTransacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brielmarca/desafio/DesafioItauTransacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brielmarca.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioItauTransacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
